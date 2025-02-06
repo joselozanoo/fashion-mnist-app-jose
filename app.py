119e8ed91e469764faa2a08efa8efb66e4bf2503
joselozanoo <joseelozanoo03@gmail.com>
2025-02-06 10:20:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = load_model('./fashion_mnist.keras')
 
 # Crear la interfaz de usuario
 st.title("Clasificador Fashion MNIST")
-st.write("Sube una imagen para clasificarla como una categoria de ropa.")
+st.write("Sube una imagen para clasificarla como una categoría de ropa.")
 
 uploaded_file = st.file_uploader("Sube una imagen en escala de grises 28x28 píxeles")
 
@@ -16,16 +16,15 @@ if uploaded_file is not None:
     # Procesar la imagen
     image = Image.open(uploaded_file).convert('L')
     image = image.resize((28, 28))
-    image_array = np.array(image) / 255.0 # Normalizar
-    # El primer uno indica que solo hay 1 image, luego las dimensiones
-    # Y el ultimo 1 indica que slolo hay un canal de color
-    image_array = image_array.reshape(1, 28, 28, 1)
+    image_array = np.array(image) / 255.0  # Normalizar
+    image_array = image_array.reshape(1, 28, 28, 1)  # Ajustar dimensiones para el modelo
 
-    #Mostrar la imagen subida
+    # Mostrar la imagen subida
     st.image(image, caption='Imagen subida', use_column_width=True)
 
     # Predicción
     prediction = model.predict(image_array)
     classes = ['Camiseta/Top', 'Pantalón', 'Suéter', 'Vestido', 'Abrigo', 'Sandalia', 'Camisa', 'Zapatilla', 'Mochila', 'Bota hasta el tobillo']
-    st.write = ("Predicción: " [np.argmax(prediction)])
-  
+    
+    # Mostrar la predicción correctamente
+    st.write(f"Predicción: {classes[np.argmax(prediction)]}")  
