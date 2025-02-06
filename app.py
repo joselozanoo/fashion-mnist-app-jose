85340ba0b74c82c5e0d458fc88155d59cfc71981
joselozanoo <joseelozanoo03@gmail.com>
2025-02-06 10:33:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 
 # Cargar el modelo
-model = load_model('./fashion_mnist.h5')
+model = load_model('./fashion_mnist.keras')
 
 # Crear la interfaz de usuario
 st.title("🛍️ Clasificador Fashion MNIST")
