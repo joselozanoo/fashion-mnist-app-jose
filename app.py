dec541e175477e4360b1a602d0580ec4e8acccae
joselozanoo <joseelozanoo03@gmail.com>
2025-02-06 10:25:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from PIL import Image
 import numpy as np
 
 # Cargar el modelo
-model = ('./fashion_mnist.keras')
+model = load_model('./fashion_mnist.keras')
 
 # Crear la interfaz de usuario
 st.title("Clasificador Fashion MNIST")
-st.write("Sube una imagen para clasificarla como una categoría de ropa.")
+st.write("Sube una imagen para clasificarla como una categoria de ropa.")
 
 uploaded_file = st.file_uploader("Sube una imagen en escala de grises 28x28 píxeles")
 
@@ -16,15 +16,21 @@ if uploaded_file is not None:
     # Procesar la imagen
     image = Image.open(uploaded_file).convert('L')
     image = image.resize((28, 28))
-    image_array = np.array(image) / 255.0  # Normalizar
-    image_array = image_array.reshape(1, 28, 28, 1)  # Ajustar dimensiones para el modelo
+    image_array = np.array(image) / 255.0 # Normalizar
+    # El primer uno indica que solo hay 1 image, luego las dimensiones
+    # Y el ultimo 1 indica que slolo hay un canal de color
+    image_array = image_array.reshape(1, 28, 28, 1)
 
-    # Mostrar la imagen subida
+    #Mostrar la imagen subida
     st.image(image, caption='Imagen subida', use_column_width=True)
 
     # Predicción
     prediction = model.predict(image_array)
     classes = ['Camiseta/Top', 'Pantalón', 'Suéter', 'Vestido', 'Abrigo', 'Sandalia', 'Camisa', 'Zapatilla', 'Mochila', 'Bota hasta el tobillo']
-    
-    # Mostrar la predicción correctamente
-    st.write(f"Predicción: {classes[np.argmax(prediction)]}")  
+    st.write = ("Predicción: " [np.argmax(prediction)])
+
+    #Mostrar Predicción
+    st.write("Predicción: ", classes[np.argmax(prediction)])
+    st.write("Probabilidad: ", np.max(prediction))
+    st.write("Probabilidades: ", prediction)
+    st.bar_chart(prediction) 
